2c1fa44b656cfeca98c82063b41b576e997634db
aply333 <andrei@aply.tech>
2024-01-07 12:36:04 -0800

    TRBLSHOOTING: 2

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const DB_HOST = process.env.STRAPI_ROOT
 
 const nextConfig = {
-  // env: {
-  //   ROOT_URL: process.env.STRAPI_ROOT
-  // },
   images: {
-    domains: [ DB_HOST, 'localhost' ]
+    domains: [ process.env.STRAPI_ROOT ]
   }
 }
 
