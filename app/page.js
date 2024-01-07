7cb39350a7155c1d1046a764a216e23df9fcce95
aply333 <andrei@aply.tech>
2024-01-07 11:46:13 -0800

    TRBLSHOOTING: 1

--- app/page.js
+++ app/page.js
@@ -3,12 +3,12 @@ import Parallax from "@/components/assets/parallax/parallax";
 import { landingGraphFetch } from "@/lib/api/strapiGraph";
 import Footer from "@/components/footer/footer";
 export default async function Home() {
-  // let landing_data = await landingGraphFetch()
+  let landing_data = await landingGraphFetch()
   return (
     <>
     <Header/>
     <main className='container'>
-
+      {/*<Parallax data={landing_data}/>*/}
     </main>
     <Footer/>
     </>
