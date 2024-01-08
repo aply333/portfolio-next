8850cd3ed9001ff6d12fe90a4f2430cb9b4725be
aply333 <andrei@aply.tech>
2024-01-07 18:54:10 -0800

    TRBLSHOOTING: 6 - Re-enabled data fetch

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Parallax from "@/components/assets/parallax/parallax";
 import { landingGraphFetch } from "@/lib/api/strapiGraph";
 import Footer from "@/components/footer/footer";
 export default async function Home() {
-  // let landing_data = await landingGraphFetch()
+  let landing_data = await landingGraphFetch()
   return (
     <>
     {/*<Header/>*/}
